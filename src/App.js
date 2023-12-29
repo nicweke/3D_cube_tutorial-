662204f729fa653af6d6fbfba0dd92c4b5f82d4a
nicweke <nicweke@gmail.com>
2023-12-29 22:29:07 +0300

    adding orbit controls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "./App.css";
-import { Canvas } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Canvas, extend, useThree } from "react-three-fiber";
 
-const Box = () => {
+extend({ OrbitControls });
+
+const Cube = (props) => {
   return (
-    <mesh rotation={[10, 10, 0]} position={[0, 0, 0]}>
+    <mesh {...props}>
       <boxGeometry attach="geometry" args={[2, 2, 2]} />
-      <meshStandardMaterial attach="material" color="#DD6031" />
+      <meshStandardMaterial attach="material" color="#69B578" />
     </mesh>
   );
 };
 
 const Scene = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
   return (
     <>
       <ambientLight />
-      <pointLight intensity={0.3} position={[-1, 2, 4]} />
-      <Box />
+      <pointLight rotation={[-1, 2, 4]} intensity={0.3} />
+      <Cube position={[0, 0, 0]} rotation={[10, 10, 0]} />
+      <Cube position={[3, 0, 0]} rotation={[10, 20, 0]} />
+      <orbitControls args={[camera, domElement]} />
     </>
   );
 };
